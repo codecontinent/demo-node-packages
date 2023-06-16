65cc605ceaae8433a0134c70a0906a378fdc03bd
Mahabub Alam <mahabubx7@gmail.com>
2023-06-16 10:17:17 +0600

    :hammer: Setup(initial):added-initial-setups

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # demo-node-packages
+
 A demo project to create and publish node packages to npm library using monorepo.
---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  collectCoverageFrom: ["packages/**/*.{ts,js,jsx}"],
+  coveragePathIgnorePatterns: ["jest.config.js", "/node_modules/", "/dist/"],
+  moduleNameMapper: {
+    "^@cc/(.*)$": "<rootDir>/packages/$1/",
+  },
+};
